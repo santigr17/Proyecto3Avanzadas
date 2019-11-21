bffa727544546a81a9abbbc8fae3b92172289e90
Santigr17 <santigr17@gmail.com>
2019-11-21 10:52:59 -0600

    First commit

---
+++ Web_App/tec-market/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Login from './Vistas/login';
+
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>    
+  );
+}
+
+export default App;
